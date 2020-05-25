71a2069557ae12fd6dc8355fac9fd58d13f4090e
Crowdbotics <team@crowdbotics.com>
2020-05-25 16:59:56 +0000

    Update models

--- tudor_and_twinkle_17377/wsgi.py
+++ tudor_and_twinkle_17377/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tudor_and_twinkle_17377.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tudor_and_twinkle_17377.settings")
 
 application = get_wsgi_application()
